02eca304bcbb10ecbaafab0e1501f87e3caa909e
Brian Faust <hello@brianfaust.me>
2017-11-05 18:24:22 +0200

    remove install notice

--- modules/config.sh
+++ modules/config.sh
@@ -11,8 +11,6 @@
 
 if [ ! -f "$directory_noah/.noah" ]; then
     cp "$directory_noah/.noah.example" "$directory_noah/.noah"
-
-    error "Yikes! You have to run noah.sh install before you can get started."
 fi
 
 if [[ -e "$directory_noah/.noah" ]]; then
