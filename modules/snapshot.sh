c20938e8f506df45aac69eb5f3a78325fa1e9c4c
Brian Faust <hello@brianfaust.me>
2017-11-23 07:06:01 +0200

    add index after snapshot restoration
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- modules/snapshot.sh
+++ modules/snapshot.sh
@@ -29,4 +29,7 @@ snapshot_download()
 snapshot_restore()
 {
     pg_restore -O -j 8 -d ark_${network} ${snapshot_dir}/current >> $noah_log 2>&1
+
+    # temporary fix to add index - https://github.com/ArkEcosystem/ark-node/pull/47
+    sudo -u postgres psql -d ark_${network} -c 'CREATE INDEX IF NOT EXISTS "mem_accounts2delegates_dependentId" ON mem_accounts2delegates ("dependentId");'
 }
