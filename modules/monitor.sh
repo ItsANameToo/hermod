0bf06821a057308d85bfa9146a838afae6ae1f1c
Brian Faust <hello@brianfaust.me>
2017-12-03 18:26:37 +0200

    add monitor_hashbangs method
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- modules/monitor.sh
+++ modules/monitor.sh
@@ -29,6 +29,7 @@ monitor()
                 fi
             fi
 
+            # Sleep if greater than 0
             if (( $monitor_rebuild > 0 )); then
                 sleep $monitor_rebuild
             fi
@@ -41,4 +42,39 @@ monitor()
     done
 
     info "Closing Monitor..."
+}
+
+monitor_hashbangs()
+{
+    heading "Starting Hashbang Monitor..."
+
+    local hashbang_pattern='############################################'
+    local hashbang_matches=5
+
+    while true; do
+        local hashbang_occurrences=$(tail -n 10 $ark_log | grep -c "$hashbang_pattern")
+
+        if [[ hashbang_occurrences -ge $hashbang_matches ]]; then
+            # Day >>> Only Notify
+            if [[ $trigger_method_notify = true && $trigger_method_rebuild = false ]]; then
+                notify "ark-node out of sync - rebuild required...";
+            fi
+
+            # Night >>> Only Rebuild
+            if [[ $trigger_method_rebuild = true ]]; then
+                if [[ $relay_enabled = true ]]; then
+                    rebuild_with_relay
+                else
+                    rebuild_via_monitor
+                fi
+            fi
+        fi
+
+        # Reduce CPU Overhead
+        if (( $monitor_interval > 0 )); then
+            sleep $monitor_interval
+        fi
+    done
+
+    info "Closing Hashbang Monitor..."
 }
