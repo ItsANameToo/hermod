995c0f8e409b8ec6003145850778137cd26e6a15
Brian Faust <hello@brianfaust.me>
2017-12-11 10:15:23 +0200

    refactor

--- modules/errors.sh
+++ modules/errors.sh
@@ -11,16 +11,11 @@
 
 abort()
 {
-    local message="Yikes! $2"
-
-    error "$message"
-    error "$message" >> $noah_log 2>&1
-
+    error "Yikes! $2" >> $noah_log 2>&1
     exit $1
 }
 
 log()
 {
-    echo "$1"
     echo "$1" >> $noah_log 2>&1
 }
