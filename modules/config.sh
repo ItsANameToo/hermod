d34399cb24bf9f0d6744daf100eb1574a623ac82
ItsANameToo <itsanametoo@protonmail.com>
2019-02-10 16:13:22 +0100

    chore: update copyright

--- modules/config.sh
+++ modules/config.sh
@@ -3,7 +3,8 @@
 # ---------------------------------------------------------------------------
 # This file is part of hermod.
 #
-# (c) ItsANameToo <itsanametoo@protonmail.com>
+# Copyright (c) 2019 ItsANameToo <itsanametoo@protonmail.com>
+# Copyright (c) Brian Faust <hello@brianfaust.me>
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
